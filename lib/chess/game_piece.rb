729b9b7a7bf328e8922350e4d26d4ec366827d7f
alvinmasaba <alvinmasaba@gmail.com>
2022-03-10 13:10:12 -0500

    Created valid_pos? as helper function for GamePiece.move

--- lib/chess/game_piece.rb
+++ lib/chess/game_piece.rb
@@ -2,11 +2,25 @@
 
 # Creates a movable chess piece
 class GamePiece
-  attr_accessor :color, :player, :transformations
+  attr_accessor :color, :player, :transformations, :position
 
-  def initialize(color, player)
+  def initialize(color, player, position)
     @color = color
     @player = player
+    @position = position
+  end
+
+  def move(dest)
+    return nil unless valid_pos?(dest)
+  end
+
+  private
+
+  def valid_pos?(position)
+    # A valid position is a string of length 2, starting with a letter between
+    # A and H and ending with an integer between 1 and 8 (inclusive).
+    return false unless position.size == 2
+
+    /[A-Ha-h][1-8]/.match?(position)
   end
 end
-  
